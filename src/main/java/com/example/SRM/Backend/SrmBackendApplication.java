8246ff30f73bbfcd8de842935e039c3e820851b6
Sandaruwan Gamage <62364412+SANDARUWANGGSS@users.noreply.github.com>
2023-03-06 20:21:18 +0530

    add rest

---
+++ src/main/java/com/example/SRM/Backend/SrmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.SRM.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrmBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/SRM/Backend/email/EmailSender.java
@@ -0,0 +1,6 @@
+package com.example.SRM.Backend.email;
+
+public interface EmailSender
+{
+    void send(String to, String email);
+}
