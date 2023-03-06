07d378a2dff353ae722d42e2dabcbaa6f4745354
Sandaruwan Gamage <62364412+SANDARUWANGGSS@users.noreply.github.com>
2023-03-06 20:12:30 +0530

    add src test files

---
+++ src/test/java/com/example/SRM/Backend/SrmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SRM.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
